1c9c17dd4f317596e03d01416431bd8993e0dc5a
jf.giammari <giammari.jf@gmail.com>
2020-11-29 17:58:34 +0100

    Update

--- Exercices/src/tp1/Main.java
+++ Exercices/src/tp1/Main.java
@@ -16,9 +16,9 @@ public class Main {
             System.out.println("Attendu : 1 --> "+m1.compareA(m2)); // Test m1 m2
             System.out.println("Attendu : 0 --> "+m1.compareA(m1)); // Test m1 m1
             System.out.println("Attendu : -1 --> "+m2.compareA(m1)); // Test m2 m1
-            System.out.println(m1.compareA(new int[]{1, 2, 3, 4})); // Test comparaison mauvais objet
+            //System.out.println(m1.compareA(new int[]{1, 2, 3, 4})); // Test comparaison mauvais objet
         }catch (ClassCastException | NullPointerException e){
-            System.out.println(e);
+            e.printStackTrace();
         }
 
 
@@ -33,13 +33,6 @@ public class Main {
             c3 = new Citerne(300, Citerne.liquid_type.WATER);
         } catch (IllegalArgumentException | IllegalCapacityException e) { System.err.println(e); }
 
-        try{
-            Citerne c4 = new Citerne(200, Citerne.liquid_type.NONE);
-            //c4.addLiquid(20); // Test d'ajout sans liquide définis
-            c1.addLiquid(20); // Ajout avec volume
-            c2.addLiquid((float)0.7); // Ajout avec poucentage
-            //c3.addLiquid(310); // Ajout avec volume trop important
-        } catch (MaxLiquidQuantityException | EmptyLiquidTypeException | IllegalCapacityException e) { System.out.println(e); }
 
         try{ // Test Equals & ToString
             assert c1 != null && c2 != null && c3 != null;
@@ -49,27 +42,41 @@ public class Main {
             System.out.println(c1);
             System.out.println(c2);
             System.out.println(c3);
-        }catch (Exception e){ System.out.println(e);}
-
-
-        try{ // Test Equals & ToString
+        }catch (Exception e){ e.printStackTrace(); }
+        
+        try{ // Test addLiquid
             assert c1 != null && c2 != null && c3 != null;
-            c1.takeLiquid(10);
-            c2.cleanCiterne();
-            c3.takeLiquid((float)0.4);
-            System.out.println(c1);
-            System.out.println(c2);
-            System.out.println(c3);
-        }catch (Exception e){ System.out.println(e);}
-/*
-
-
-
-            c2.changeLiquid(Citerne.liquid_type.WATER);
-            c1.cleanCiterne();
-            c1.changeLiquid(Citerne.liquid_type.VINE);
+            Citerne c4 = new Citerne(200, Citerne.liquid_type.NONE);
+            //c4.addLiquid(20); // Ajout sans liquide définis
+            c1.addLiquid(20); // Ajout avec volume
+            c2.addLiquid((float)0.7); // Ajout avec poucentage
+            c3.addLiquid(300);
+            //c3.addLiquid(10); // Ajout avec volume trop important
             System.out.println(c1.getContenu());
- */
+            System.out.println(c2.getContenu());
+            System.out.println(c3.getContenu());
+        } catch (MaxLiquidQuantityException | EmptyLiquidTypeException | IllegalCapacityException e) { e.printStackTrace(); }
+        
+        try{ // Test takeLiquid
+            assert c1 != null && c2 != null && c3 != null;
+            c1.takeLiquid(10); // Prendre avec volume
+            c3.takeLiquid((float)0.4); // Prendre avec poucentage
+            //c2.takeLiquid(500); // Prendre trops
+            System.out.println(c1.getContenu());
+            System.out.println(c2.getContenu());
+            System.out.println(c3.getContenu());
+        }catch (MinLiquidQuantityException e) { e.printStackTrace(); }
+
+
+        try{ // Test changeLiquid & cleanCiterne
+            assert c1 != null && c2 != null && c3 != null;
+            System.out.println(c2.getContenu());
+            //c2.changeLiquid(Citerne.liquid_type.WATER); //  Changement sans cleanCiterne
+            c2.cleanCiterne(); // cleanCiterne
+            c2.changeLiquid(Citerne.liquid_type.VINE); // Changement apres cleanCiterne
+            System.out.println(c2.getContenu());
+        }catch (IsNotCleanException e) { e.printStackTrace(); }
+
 
 
 
